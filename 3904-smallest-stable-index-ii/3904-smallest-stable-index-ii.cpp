18e15eabdfc77e4a700d9b6e453c8159c026d371
Abhijeet Fulpagare <fulpagareabhijeet39@gmail.com>
2026-09-05 13:18:49 +0530

    Time: 15 ms (39.37%), Space: 210.9 MB (35.81%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+    vector<int> findpge(vector<int>&nums,int &n)
+    {
+        vector<int>ans(n,0);
+
+        int t = -1;
+        
+        for(int i=0 ; i<n ; i++)
+        {
+            t = max(t,nums[i]);
+            ans[i] = t;
+        }
+
+        return ans;
+    }
+
+    vector<int> findnse(vector<int>&nums,int &n)
+    {
+        vector<int>ans(n,0);
+
+        int t = INT_MAX;
+        
+        for(int i=n-1 ; i>=0 ; i--)
+        {
+            t = min(t,nums[i]);
+            ans[i] = t;
+        }
+
+        return ans;
+    }
+
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int>pge,nse;
+
+        pge = findpge(nums,n);
+
+        nse = findnse(nums,n);
+
+        int ans = INT_MAX;
+        for(int i=0 ; i<n ; i++)
+        {
+            if((pge[i] - nse[i]) <= k )
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
